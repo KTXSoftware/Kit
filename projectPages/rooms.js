b949f588742e3588921eed7dce0171b8d31fa0a4
Robert Konrad <robert@ktx-software.com>
2014-01-05 17:18:04 +0100

    Proper room asset handling.

--- projectPages/rooms.js
+++ projectPages/rooms.js
@@ -18,32 +18,95 @@ function findAsset(kha, assetId) {
 	}
 }
 
-function loadAssets(kha, room, type, title) {
+function contains(room, asset) {
+	for (var assetId in room.assets) {
+		if (asset.id === room.assets[assetId]) return true;
+	}
+	return false;
+}
+
+function arrayRemove(array, at) {
+	var rest = array.slice(at + 1);
+	array.length = at < 0 ? array.length + at : at;
+	return array.push.apply(array, rest);
+}
+
+function remove(room, asset, table, line) {
+	for (var assetId in room.assets) {
+		if (asset.id === room.assets[assetId]) {
+			arrayRemove(room.assets, room.assets.indexOf(room.assets[assetId]));
+			return;
+		}
+	}
+}
+
+function addLine(kha, room, asset, table) {
+	let line = document.createElement("tr");
+	let td = document.createElement("td");
+	td.appendChild(document.createTextNode(asset.name));
+	line.appendChild(td);
+
+	td = document.createElement("td");
+	var button = document.createElement("button");
+	button.appendChild(document.createTextNode("-"));
+	button.onclick = function() {
+		remove(room, asset, table, line);
+		loadRoom(kha, room);
+	};
+	td.appendChild(button);
+	line.appendChild(td);
+	table.appendChild(line);
+}
+
+function loadAssets(kha, room, type, title, table) {
+	var tr = document.createElement("tr");
+	var td = document.createElement("td");
+
 	var h = document.createElement("h3");
 	h.appendChild(document.createTextNode(title));
-	roomsColumn.appendChild(h);
+	td.appendChild(h);
+	tr.appendChild(td);
+	table.appendChild(tr);
+
+	tr = document.createElement("tr");
+	td = document.createElement("td");
+	var select = document.createElement("select");
+	
+	for (var asset in kha.assets) {
+		if (kha.assets[asset].type === type && !contains(room, kha.assets[asset])) {
+			var option = document.createElement("option");
+			option.appendChild(document.createTextNode(kha.assets[asset].name));
+			option.setAttribute("value", kha.assets[asset].id);
+			select.appendChild(option);
+		}
+	}
+
+	select.onchange = function() {
+		var value = select.options[select.selectedIndex].value;
+		room.assets.push(value);
+		loadRoom(kha, room);
+	};
+	
+	td.appendChild(select);
+	tr.appendChild(td);
+	table.appendChild(tr);
 
 	for (var id in room.assets) {
-		var asset = findAsset(kha, room.assets[id]);
+		let asset = findAsset(kha, room.assets[id]);
 		if (asset.type === type) {
-			roomsColumn.appendChild(document.createTextNode(asset.name));
-			var button = document.createElement("button");
-			button.appendChild(document.createTextNode("-"));
-			button.onclick = function() {
-				
-			};
-			roomsColumn.appendChild(button);
-			roomsColumn.appendChild(document.createElement("br"));
+			addLine(kha, room, asset, table);
 		}
 	}
 }
 
 function loadRoom(kha, room) {
 	clear(roomsColumn);
-	loadAssets(kha, room, "image", "Images");
-	loadAssets(kha, room, "sound", "Sounds");
-	loadAssets(kha, room, "music", "Music");
-	loadAssets(kha, room, "blob", "Blobs");
+	var table = document.createElement("table");
+	loadAssets(kha, room, "image", "Images", table);
+	loadAssets(kha, room, "sound", "Sounds", table);
+	loadAssets(kha, room, "music", "Music", table);
+	loadAssets(kha, room, "blob", "Blobs", table);
+	roomsColumn.appendChild(table);
 }
 
 exports.load = function(repository, kha, element) {
