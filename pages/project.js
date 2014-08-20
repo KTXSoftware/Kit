a3622c79830270fd29b2864cfe00b293b68f46d8
Robert Konrad <robert@ktx-software.com>
2014-08-20 05:15:03 +0200

    React assets.

--- pages/project.js
+++ pages/project.js
@@ -1,36 +1,28 @@
-define(['../config.js', '../react.js', '../projectPages/project.js'], function (config, React, ProjectPage) {
+define(['../config.js', '../react.js', '../projectPages/project.js', '../projectPages/assets.js'], function (config, React, ProjectPage, AssetsPage) {
 	var fs = require('fs');
-	//var projectPage = require("../projectPages/project.js");
-	//var assetsPage  = require("../projectPages/assets.js");
 	//var roomsPage   = require("../projectPages/rooms.js");
 
 	return React.createClass({displayName: 'Project',
 		getInitialState: function () {
 			if (fs.existsSync(config.projectsDirectory() + "/" + this.props.name + "/project.kha")) {
-				var kha = JSON.parse(fs.readFileSync(config.projectsDirectory() + "/" + this.props.name + "/project.kha", {encoding: "utf8"}));
+				this.kha = JSON.parse(fs.readFileSync(config.projectsDirectory() + "/" + this.props.name + "/project.kha", {encoding: "utf8"}));
 				var self = this;
-				kha.save = function () {
+				this.kha.save = function () {
 					var string = JSON.stringify(kha, null, "\t");
 					fs.writeFileSync(config.projectsDirectory() + "/" + self.props.name + "/project.kha", string, {encoding: "utf8"});
 				};
 			}
-			//projectButton.onclick = function() {
-			//	clear(td);
-			//	projectPage.load(repository, kha, td);
-			//};
-			//assetsButton.onclick = function() {
-			//	clear(td);
-			//	assetsPage.load(repository, kha, td);
-			//};
 			//roomsButton.onclick = function() {
 			//	clear(td);
 			//	roomsPage.load(repository, kha, td);
 			//};
-			//projectPage.load(repository, kha, td);
 			return {page: 'Project'};
 		},
 		loadProject: function () {
 			this.setState({page: 'Project'});
+		},
+		loadAssets: function () {
+			this.setState({page: 'Assets'});
 		},
 		render: function () {
 			var page = null;
@@ -38,18 +30,23 @@ define(['../config.js', '../react.js', '../projectPages/project.js'], function (
 				case 'Project':
 					page = ProjectPage({repository: this.props.name});
 					break;
+				case 'Assets':
+					page = AssetsPage({kha: this.kha});
+					break;
 			}
 			return (
 				React.DOM.table(null,
-					React.DOM.tr(null,
-						React.DOM.td(null,
-							React.DOM.button({onClick: this.loadProject}, this.props.name),
-							React.DOM.button(null, 'Assets'),
-							React.DOM.button(null, 'Rooms')
+					React.DOM.tbody(null,
+						React.DOM.tr(null,
+							React.DOM.td(null,
+								React.DOM.button({onClick: this.loadProject}, this.props.name),
+								React.DOM.button({onClick: this.loadAssets}, 'Assets'),
+								React.DOM.button(null, 'Rooms')
+							)
+						),
+						React.DOM.tr(null,
+							React.DOM.td(null, page)
 						)
-					),
-					React.DOM.tr(null,
-						React.DOM.td(null, page)
 					)
 				)
 			);
--- projectPages/assets.js
+++ projectPages/assets.js
@@ -1,153 +1,108 @@
 "use strict";
 
-var config = require("../config.js");
-var fs = require("fs");
-var uuid = require("node-uuid");
+define(['../config.js', '../react.js'], function (config, React) {
+	var fs = require("fs");
+	var uuid = require("node-uuid");
 
-var document = window.document;
-
-function clear(element) {
-	while (element.lastChild) {
-		element.removeChild(element.lastChild);
+	function remove(array, at) {
+		var rest = array.slice(at + 1);
+		array.length = at < 0 ? array.length + at : at;
+		return array.push.apply(array, rest);
 	}
-}
 
-function remove(array, at) {
-	var rest = array.slice(at + 1);
-	array.length = at < 0 ? array.length + at : at;
-	return array.push.apply(array, rest);
-}
+	function copyFile(from, to) {
+		var data = fs.readFileSync(from);
+		fs.writeFileSync(to, data);
+	}
 
-function loadAssets(kha, assets, type, element) {
-	clear(element);
-	var table = document.createElement("table");
-	for (var asset in assets) {
-		let current = assets[asset];
-		if (assets[asset].type === type) {
-			var tr = document.createElement("tr");
-			var td = document.createElement("td");
+	return React.createClass({displayName: 'AssetsPage',
+		getInitialState: function () {
+			return {type: 'image'};
+		},
+		selection: function (event) {
+			this.setState({type: event.target.value});
+		},
+		render: function () {
+			var rows = [];
+			for (var asset in this.props.kha.assets) {
+				let current = this.props.kha.assets[asset];
+				if (current.type === this.state.type) {
+					var tr = React.DOM.tr(null,
+						React.DOM.td(null,
+							React.DOM.a({href: '#', onClick: function () { return false; }}, current.name)
+						),
+						React.DOM.td(null,
+							React.DOM.button(null, '-')
+						)
+					);
 
-			var a = document.createElement("a");
-			a.setAttribute("href", "#");
-			a.onclick = function() {
-				//window.alert("click");
-				return false;
-			};
-			a.appendChild(document.createTextNode(assets[asset].name));
-			td.appendChild(a);
-			tr.appendChild(td);
-
-			td = document.createElement("td");
-			var button = document.createElement("button");
-			button.appendChild(document.createTextNode("-"));
-			button.onclick = function() {
-				for (var asset in assets) {
-					if (assets[asset].type === type && assets[asset].name === current.name) {
-						remove(assets, assets.indexOf(assets[asset]));
-						kha.save();
-						loadAssets(assets, type, element);
-						return;
-					}
+					/*button.onclick = function() {
+						for (var asset in assets) {
+							if (assets[asset].type === type && assets[asset].name === current.name) {
+								remove(assets, assets.indexOf(assets[asset]));
+								kha.save();
+								loadAssets(assets, type, element);
+								return;
+							}
+						}
+					};*/
+					rows.push(tr);
 				}
-			};
-			td.appendChild(button);
-			tr.appendChild(td);
-			table.appendChild(tr);
+			}
+			return (
+				React.DOM.div(null,
+					React.DOM.select({value: 'image', onChange: this.selection},
+						React.DOM.option({value: 'image'}, 'Images'),
+						React.DOM.option({value: 'music'}, 'Music'),
+						React.DOM.option({value: 'sound'}, 'Sounds'),
+						React.DOM.option({value: 'blob'}, 'Blobs')
+					),
+					React.DOM.br(null),
+					React.DOM.input({type: 'file'}),
+					React.DOM.div(null,
+						React.DOM.table(null, React.DOM.tbody(null, rows))
+					)
+				)
+			);
 		}
-	}
-	element.appendChild(table);
-}
+	});
 
-function copyFile(from, to) {
-	var data = fs.readFileSync(from);
-	fs.writeFileSync(to, data);
-}
-
-exports.load = function(repository, kha, element) {
-	var select = document.createElement("select");
-
-	var option = document.createElement("option");
-	option.appendChild(document.createTextNode("Images"));
-	select.appendChild(option);
-	
-	option = document.createElement("option");
-	option.appendChild(document.createTextNode("Music"));
-	select.appendChild(option);
-	
-	option = document.createElement("option");
-	option.appendChild(document.createTextNode("Sounds"));
-	select.appendChild(option);
-
-	option = document.createElement("option");
-	option.appendChild(document.createTextNode("Blobs"));
-	select.appendChild(option);
-
-	var div = document.createElement("div");
-	
-	select.onchange = function() {
-		switch (select.selectedIndex) {
-		case 0:
-			loadAssets(kha, kha.assets, "image", div);
-			break;
-		case 1:
-			loadAssets(kha, kha.assets, "music", div);
-			break;
-		case 2:
-			loadAssets(kha, kha.assets, "sound", div);
-			break;
-		case 3:
-			loadAssets(kha, kha.assets, "blob", div);
-			break;
-		}
-	};
-
-	element.appendChild(select);
-
-	element.appendChild(document.createElement("br"));
-
-	var button = document.createElement("input");
-	button.appendChild(document.createTextNode("Add Image..."));
-	button.setAttribute("type", "file");
-	button.onchange = function() {
-		var dir = "/Assets/"
-		switch (select.selectedIndex) {
-		case 0:
-			dir += "Graphics/";
-			break;
-		case 1:
-		case 2:
-			dir += "Sound/";
-			break;
-		}
-		var value = button.value.replace(/\\/g, "/");
-		var name = value.substr(value.lastIndexOf("/") + 1);
-		copyFile(value, config.projectsDirectory() + "/" + repository + dir + name);
-		var shortname = name.substring(0, name.lastIndexOf("."));
-		var type = "image";
-		switch (select.selectedIndex) {
-		case 1:
-			type = "music";
-			break;
-		case 2:
-			type = "sound";
-			break;
-		case 3:
-			type = "blob";
-			break;
-		}
-		kha.assets.push( {
-			id: uuid.v4(),
-      		type: type,
-      		file: name,
-      		name: shortname
-		});
-		kha.save();
-		loadAssets(kha, kha.assets, type, div);
-	};
-	element.appendChild(button);
-	element.appendChild(div);
-
-	if (kha !== undefined) {
-		loadAssets(kha, kha.assets, "image", div);
-	}
-}
+	/*
+		button.onchange = function() {
+			var dir = "/Assets/"
+			switch (select.selectedIndex) {
+			case 0:
+				dir += "Graphics/";
+				break;
+			case 1:
+			case 2:
+				dir += "Sound/";
+				break;
+			}
+			var value = button.value.replace(/\\/g, "/");
+			var name = value.substr(value.lastIndexOf("/") + 1);
+			copyFile(value, config.projectsDirectory() + "/" + repository + dir + name);
+			var shortname = name.substring(0, name.lastIndexOf("."));
+			var type = "image";
+			switch (select.selectedIndex) {
+			case 1:
+				type = "music";
+				break;
+			case 2:
+				type = "sound";
+				break;
+			case 3:
+				type = "blob";
+				break;
+			}
+			kha.assets.push( {
+				id: uuid.v4(),
+	      		type: type,
+	      		file: name,
+	      		name: shortname
+			});
+			kha.save();
+			loadAssets(kha, kha.assets, type, div);
+		};
+	}*/
+});
