85adaf0a9c4ae1f654e3a5214eddb55a56ca2a8c
Robert Konrad <robert@ktx-software.com>
2014-08-21 03:25:03 +0200

    Fixed config page.

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ define(function () {
 		aacencoder: '',
 		hideUnavailable: false,
 		git: 'git',
+		visualStudio: 'vs2013',
+		windowsGraphics: 'direct3d9',
 		servers: [
 			{
 				name: 'ktx-github',
@@ -37,6 +39,8 @@ define(function () {
 			options = JSON.parse(fs.readFileSync(optionsFile, {encoding: 'utf8'}));
 			if (options.hideUnavailable === undefined) options.hideUnavailable = false;
 			if (options.git === undefined) options.git = 'git';
+			if (options.visualStudio === undefined) options.visualStudio = 'vs2013';
+			if (options.windowsGraphics === undefined) options.windowsGraphics = 'direct3d9';
 			for (var s in options.servers) {
 				var server = options.servers[s];
 				if (server.name === undefined) server.name = 'ktx-github';
@@ -71,66 +75,84 @@ define(function () {
 		optionsPath = dataPath + path.sep;
 		optionsFile = optionsPath + 'options.json';
 		load();
-	}
+	};
 
 	exports.projectsDirectory = function () {
 		return options.projectsDirectory;
-	}
+	};
 
 	exports.mp3Encoder = function () {
 		return options.mp3encoder;
-	}
+	};
 
 	exports.aacEncoder = function () {
 		return options.aacencoder;
-	}
+	};
 
 	exports.servers = function () {
 		return options.servers;
-	}
+	};
 
 	exports.serverData = function (serverName) {
 		return serverData[serverName];
-	}
+	};
 
 	exports.hideUnavailable = function () {
 		return options.hideUnavailable;
-	}
+	};
 
 	exports.git = function () {
 		return options.git;
-	}
+	};
+
+	exports.visualStudio = function () {
+		return options.visualStudio;
+	};
+
+	exports.windowsGraphics = function () {
+		return options.windowsGraphics;
+	};
 
 	exports.setProjectsDirectory = function (dir) {
 		options.projectsDirectory = dir;
 		save();
-	}
+	};
 
 	exports.setMP3Encoder = function (text) {
 		options.mp3encoder = text;
 		save();
-	}
+	};
 
 	exports.setAACEncoder = function (text) {
 		options.aacencoder = text;
 		save();
-	}
+	};
 
 	exports.setHideUnavailable = function (hide) {
 		options.hideUnavailable = hide;
 		save();
-	}
+	};
 
 	exports.setGit = function (git) {
 		options.git = git;
 		save();
-	}
+	};
+
+	exports.setVisualStudio = function (visualStudio) {
+		options.visualStudio = visualStudio;
+		save();
+	};
+
+	exports.setWindowsGraphics = function (windowsGraphics) {
+		options.windowsGraphics = windowsGraphics;
+		save();
+	};
 
 	exports.saveServerData = function (serverName) {
 		fs.writeFile(optionsPath + serverName + '.json', JSON.stringify(serverData[serverName], null, '\t'), {encoding: 'utf8'}, function (err) {
 			// TODO: log error?
 		});
-	}
+	};
 
 	return exports;
 });
--- pages/config.js
+++ pages/config.js
@@ -1,16 +1,22 @@
 define(['../log.js', '../react.js', '../config.js'], function (log, React, config) {
 	return React.createClass({displayName: 'Config',
-		projectsDir: function () {
-			config.setProjectsDirectory(this.value);
+		projectsDir: function (event) {
+			config.setProjectsDirectory(event.target.value);
 		},
-		mp3: function () {
-			config.setMP3Encoder(this.value);
+		mp3: function (event) {
+			config.setMP3Encoder(event.target.value);
 		},
-		aac: function () {
-			config.setAACEncoder(this.value);
+		aac: function (event) {
+			config.setAACEncoder(event.target.value);
 		},
-		git: function () {
-			config.setGit(this.value);
+		git: function (event) {
+			config.setGit(event.target.value);
+		},
+		visualStudio: function (event) {
+			config.setVisualStudio(event.target.value);
+		},
+		graphics: function (event) {
+			config.setWindowsGraphics(event.target.value);
 		},
 		render: function () {
 			return (
@@ -21,21 +27,39 @@ define(['../log.js', '../react.js', '../config.js'], function (log, React, confi
 					),
 					React.DOM.tr(null,
 						React.DOM.td(null, 'MP3 encoder'),
-						React.DOM.td(null, React.DOM.input({value: config.mp3Encoder(), onChange: this.mp3}))
+						React.DOM.td(null, React.DOM.input({defaultValue: config.mp3Encoder(), onChange: this.mp3}))
 					),
 					React.DOM.tr(null,
 						React.DOM.td(null, 'AAC encoder'),
-						React.DOM.td(null, React.DOM.input({value: config.aacEncoder(), onChange: this.aac}))
+						React.DOM.td(null, React.DOM.input({defaultValue: config.aacEncoder(), onChange: this.aac}))
 					),
 					React.DOM.tr(null,
 						React.DOM.td(null, 'Git command'),
-						React.DOM.td(null, React.DOM.input({value: config.git(), onChange: this.git}))
+						React.DOM.td(null, React.DOM.input({defaultValue: config.git(), onChange: this.git}))
+					),
+					React.DOM.tr(null,
+						React.DOM.td(null, 'Visual Studio'),
+						React.DOM.td(null, 
+							React.DOM.select({defaultValue: config.visualStudio(), onChange: this.visualStudio},
+								React.DOM.option({value: 'vs2010'}, '2010'),
+								React.DOM.option({value: 'vs2012'}, '2012'),
+								React.DOM.option({value: 'vs2013'}, '2013')
+							)
+						)
+					),
+					React.DOM.tr(null,
+						React.DOM.td(null, 'Windows Graphics'),
+						React.DOM.td(null,
+							React.DOM.select({defaultValue: config.windowsGraphics(), onChange: this.graphics},
+								React.DOM.option({value: 'direct3d9'}, 'Direct3D 9'),
+								React.DOM.option({value: 'direct3d11'}, 'Direct3D 11'),
+								React.DOM.option({value: 'opengl2'}, 'OpenGL')
+							)
+						)
 					)
 				)
 			);
 			//table.appendChild(createRow2(document.createTextNode("Android Devkit"), document.createTextNode("_")));
-			//table.appendChild(createRow2(document.createTextNode("Visual Studio"), document.createTextNode("_")));
-			//table.appendChild(createRow2(document.createTextNode("Windows Graphics"), document.createTextNode("_")));
 			//table.appendChild(createRow2(document.createTextNode("Precompiled Headers"), document.createTextNode("_")));
 			//table.appendChild(createRow2(document.createTextNode("Intermediate Drive"), document.createTextNode("_")));
 		}
